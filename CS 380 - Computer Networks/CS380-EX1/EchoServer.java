cf1bb3895717c4e00bb8127b96134da926120784
Jarod Nakamoto <jknakamoto@cpp.edu>
2017-09-28 00:04:06 -0700

    Working Version of Threads

--- CS 380 - Computer Networks/CS380-EX1/EchoServer.java
+++ CS 380 - Computer Networks/CS380-EX1/EchoServer.java
@@ -7,76 +7,79 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.Random;
+import java.io.IOException;
 
 public final class EchoServer {
 
+	public static volatile Socket socket;
+
     public static void main(String[] args) throws Exception {
-        try (ServerSocket serverSocket = new ServerSocket(22222)) {
+        Runnable clientHandler = () -> { 
+                
+					String address = socket.getInetAddress().getHostAddress();
+					//prints to console client joined
+					System.out.printf("Client connected: %s%n", address);
+					
+					//creates the runnable for client
+					//new RequestHandler(socket);
+					//Random random = new Random();
+					try{
+						//message receivers
+						InputStream is = socket.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+						//recieves messages
+						BufferedReader br = new BufferedReader(isr);
+						OutputStream os = socket.getOutputStream();
+						//sends messages to client
+						PrintStream out = new PrintStream(os, true, "UTF-8");
+						
+						//while connected to client
+						while(!socket.isClosed())
+						{
+							String msg = br.readLine();
+							if(msg.equals("exit"))
+								socket.close();
+							else
+								out.println(msg);
+							/*
+							try{
+								Thread.sleep(1000);
+								//Thread.sleep(random.nextInt(1000));
+							}
+							catch(InterruptedException e){
+								return;
+							}*/		
+						}
+						//prints to console client left
+						System.out.printf("Client disconnected: %s%n", address);
+					}
+					catch(Exception e)
+					{
+						System.out.println(e.toString());
+					}
+				
+				
+                };try (ServerSocket serverSocket = new ServerSocket(22222)) {
 			//server runs indefinitely
             while (true) {
 				//establish connection
-                try (Socket socket = serverSocket.accept()) {
-					
-					//creates the runnable for client
-					Runnable client = () -> {
-						Random random = new Random();
-						try{
-							System.out.println("Inside Runnable");	
-							//message receivers
-							InputStream is = socket.getInputStream();
-							
-							System.out.println("after first call to socket");
-							
-							InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-							//recieves messages
-							BufferedReader br = new BufferedReader(isr);
-							String address = socket.getInetAddress().getHostAddress();
-							//prints to console client joined
-							System.out.printf("Client connected: %s%n", address);
-							OutputStream os = socket.getOutputStream();
-							//sends messages to client
-							PrintStream out = new PrintStream(os, true, "UTF-8");
-							
-							System.out.println("After Initializations");	
-							
-							//while connected to client
-							while(!socket.isClosed())
-							{
-								String msg = br.readLine();
-								if(msg.equals("exit"))
-									socket.close();
-								else
-									out.println(msg);
-								
-								try{
-									Thread.sleep(random.nextInt(1000));
-								}
-								catch(InterruptedException e){
-									return;
-								}
-								
-							}
-							
-							//prints to console client left
-							System.out.printf("Client disconnected: %s%n", address);
-						}
-						catch(Exception e)
-						{
-							System.out.println(e.toString());
-						}
-					};
-					
-					//creates thread objects to execute Runnable
-					Thread client1 = new Thread(client());
-					//Thread client2 = new Thread(client);
-					
-					//starts threads. Main thread countinues
-					client1.start();
-					//client2.start();
-                }
-				
-				Thread.sleep(1000);
+				try {
+					socket = serverSocket.accept();
+				}
+				catch(IOException ioE)
+				{
+					System.out.println(ioE.toString());
+				}
+				//creates thread objects to execute Runnable
+				//starts threads. Main thread countinues
+				Thread client1 = new Thread(clientHandler);
+				//Thread client2 = new Thread(clientHandler);
+				client1.start();
+				//client2.start();
+				//Thread.sleep(1000);
             }
+			
+					
         }
     }
 }
