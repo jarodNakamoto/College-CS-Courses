7217b69e208fb1013f541cc6efdbb4b6d56ed772
Jarod Nakamoto <jknakamoto@cpp.edu>
2017-10-11 17:51:26 -0700

    CS380-EX2 Version 1

---
+++ CS 380 - Computer Networks/CS380-EX2/Ex2Client.java
@@ -0,0 +1,74 @@
+
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Random;
+import java.io.IOException;
+import java.util.zip.CRC32;
+
+public final class Ex2Client {
+
+    public static void main(String[] args) throws Exception {
+		
+		try (Socket socket = new Socket("18.221.102.182", 38102)) {
+			
+			//display that server connection was successful
+			String address = socket.getInetAddress().getHostAddress();
+			System.out.printf("Connected to: %s%n", address);
+			
+			//recieves bytes
+            InputStream is = socket.getInputStream();
+			
+			System.out.print("Received bytes:");
+			byte[] bytesTranslated = new byte[100];
+			
+			for(int i = 0; i < 100; i++)
+			{
+				if(i%10 == 0)
+				{
+					System.out.println();
+					System.out.print("   ");
+				}
+				int halfByte1 = is.read();
+				int halfByte2 = is.read();
+				
+				halfByte1 = halfByte1 << 4;
+				bytesTranslated[i] = (byte)(halfByte1 ^ halfByte2);
+				System.out.print(String.format("%02X", bytesTranslated[i]));
+			}
+			
+			//create CRC-32 value
+			CRC32 crc32 = new CRC32();
+			crc32.update(bytesTranslated);
+			
+			Long crcVal = new Long(crc32.getValue());
+			byte[] crcValBytes = new byte[4];
+			Long copy = new Long(crcVal.longValue());
+			
+			//take the long and make it into four bytes
+			for(int i = 0; i < 4; i++)
+			{	
+				crcValBytes[i] = copy.byteValue();
+				copy = copy >> 4;
+			}
+			
+			System.out.println("\nGenerated CRC32: " + String.format("%08X", crcVal.intValue()) +".");
+			
+			//sends bytes to server
+			OutputStream os = socket.getOutputStream();
+			os.write(crcValBytes);
+			
+			//receive if crc is correct
+			if(is.read() == 1)
+				System.out.println("Response good");
+			else
+				System.out.println("Response good");
+			System.out.println("Disconnected from server.");
+			is.close();
+        }
+	}
+}
---
+++ CS 380 - Computer Networks/CS380-EX2/test.java
@@ -0,0 +1,32 @@
+
+public class test
+{
+	//class to test how bytes longs and others work 
+	public static void main(String[] args)
+	{
+		/*
+		byte b1 = 0x05;
+		byte b2 = 0x0A;
+		byte b3 = 0x5A;
+		
+		byte temp = b1;
+		temp = (byte)(temp << 4);
+		
+		byte b4 = (byte)(temp ^ b2);
+		
+		System.out.println("b1: " + String.format("%02X",b1));
+		System.out.println("b2: " + String.format("%02X",b2));
+		System.out.println("b3: " + String.format("0x%02X",b3));
+		System.out.println("b4: " + String.format("0x%02X",b4));
+	*/
+	
+		long l = 0XFA128A79;
+		byte lB = ((Long)l).byteValue();
+		System.out.println("l: " + String.format("%08X", l));
+		System.out.println("l's byte value: " + String.format("%08X", lB));
+		/* test output
+		l: FFFFFFFFFA128A79
+		l's byte value: 00000079
+		*/
+	}
+}
